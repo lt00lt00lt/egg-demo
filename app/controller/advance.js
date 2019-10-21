6326b24ae9a2c320db3d3ab27439ce38c38c8e47
494858549@qq.com <494858549@qq.com>
2019-10-21 19:57:19 +0800

    '新增通用增删改功能接口，批量增加和批量删除还在调试'

--- app/controller/advance.js
+++ app/controller/advance.js
@@ -3,11 +3,48 @@
 const Controller = require('egg').Controller;
 
 class AdvanceController extends Controller {
+    //检索方法
     async find() {
         const { ctx, service } = this;
         let tableName = ctx.params.tableName;
         let conditions = ctx.request.body;
         const res = await service.advance.find(tableName, conditions);
+        ctx.body = res;
+    }
+
+    //删除方法
+    async delete() {
+        const { ctx, service } = this;
+        let tableName = ctx.params.tableName;
+        let id = ctx.params.id;
+        const res = await service.advance.delete(tableName, id);
+        ctx.body = res;
+    }
+
+    //批量删除方法
+    async batchDelete() {
+        const { ctx, service } = this;
+        let tableName = ctx.params.tableName;
+        let ids = ctx.request.body;
+        const res = await service.advance.batchDelete(tableName, ids);
+        ctx.body = res;
+    }
+
+    //保存方法
+    async save() {
+        const { ctx, service } = this;
+        let tableName = ctx.params.tableName;
+        let datas = ctx.request.body;
+        const res = await service.advance.save(tableName, datas);
+        ctx.body = res;
+    }
+
+    //批量保存方法
+    async batchSave() {
+        const { ctx, service } = this;
+        let tableName = ctx.params.tableName;
+        let datas = ctx.request.body;
+        const res = await service.advance.batchSave(tableName, datas);
         ctx.body = res;
     }
 }
--- app/service/advance.js
+++ app/service/advance.js
@@ -1,12 +1,55 @@
 'use strict';
 
 const Service = require('egg').Service;
+const uuid = require('uuid/v1');
 
 class AdvanceService extends Service {
+    //检索数据
     async find(tableName, conditions) {
-        console.log(conditions);
         const results = await this.app.mysql.select(tableName);
         return { results };
+    }
+
+    //根据id删除数据
+    async delete(tableName, id) {
+        const results = await this.app.mysql.delete(tableName, { id: id });
+    }
+
+    //根据id批量删除数据
+    async batchDelete(tableName, ids) {
+        ids.forEach(element => {
+            await this.app.mysql.delete(tableName, { id: element });
+        });
+    }
+
+    //保存数据
+    async save(tableName, datas) {
+        //如果参数中传入了id，则为修改，否则为插入
+        if (datas.id) {
+            const result = await this.app.mysql.update(tableName, datas); // 更新 posts 表中的记录
+            const updateSuccess = result.affectedRows === 1;
+        } else {
+            datas["id"] = uuid();
+            const result = await this.app.mysql.insert(tableName, datas);
+            // 判断插入成功·
+            const insertSuccess = result.affectedRows === 1;
+        }
+    }
+
+    //批量保存数据
+    async batchSave(tableName, datas) {
+        datas.forEach(element => {
+            //如果参数中传入了id，则为修改，否则为插入
+            if (element.id) {
+                const result = await this.app.mysql.update(tableName, element); // 更新 posts 表中的记录
+                const updateSuccess = result.affectedRows === 1;
+            } else {
+                element["id"] = uuid();
+                const result = await this.app.mysql.insert(tableName, element);
+                // 判断插入成功·
+                const insertSuccess = result.affectedRows === 1;
+            }
+        });
     }
 }
 
